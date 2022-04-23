fc2c6baee84e0920e551298fe90d87ebe2497e91
NDT <ngocductran2000@gmail.com>
2022-04-23 21:37:14 +0700

    push code

---
+++ resources/views/index.blade.php
@@ -0,0 +1,312 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Giới Trẻ Công Giáo</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="{{ asset('public/frontend/css/style.css') }}">
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+  <link rel='stylesheet prefetch' href='http://fonts.googleapis.com/css?family=Roboto:400,100,400italic,700italic,700'>
+</head>
+<body>
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-light fixed-top bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+          <img class="img-logo" src="https://www.tonggiaophanhanoi.org/wp-content/uploads/2020/12/logo_150-80x85.png" alt="">
+          <strong class="brand" style="color: red;">Tổng Giáo Phận Hà Nội</strong>
+        </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Tin Tức</a>
+           <ul class="sub-menu bg-light">
+            <li><a href="#" title="Lập trình C/C++">Lập trình C/C++</a></li>
+            <li><a href="#" title="Lập trình Web PHP&MySQL">Lập trình Web</a></li>
+            <li><a href="#" title="Lập trình Web PHP&MySQL">Lập trình PHP&MySQL</a></li>
+            <li><a href="#" title="Lập trình Web PHP&MySQL">Lập trình Python</a></li>
+            <li><a href="#" title="Lập trình Web PHP&MySQL">Lập trình Java</a></li>
+           </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Phụng Vụ</a>
+          <ul class="sub-menu bg-light">
+            <li><a href="#" title="Lập trình C/C++">Lập trình C/C++</a></li>
+            <li><a href="#" title="Lập trình Web PHP&MySQL">Lập trình Web</a></li>
+            <li><a href="#" title="Lập trình Web PHP&MySQL">Lập trình PHP&MySQL</a></li>
+            <li><a href="#" title="Lập trình Web PHP&MySQL">Lập trình Python</a></li>
+            <li><a href="#" title="Lập trình Web PHP&MySQL">Lập trình Java</a></li>
+           </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Giáo Lý</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Cộng Đoàn</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Liên Hệ</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<!-- Page Content -->
+<div class="container menu-padding-top">
+  <div class="bg-primary div-inline">
+  </div>
+  <img class="img-fluid" style="border-radius: 15px;padding: 5px;" width="100%" height="120%" src="https://www.tonggiaophanhanoi.org/wp-content/uploads/2021/07/le-truc-tuyen.png">
+  <div class="bg-primary div-inline">
+  </div>
+  <div class="row">
+    <div class="col-md-8 col-sm-12 text-center">
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block w-100 img-sline" src="https://massageishealthy.com/wp-content/uploads/2019/06/hinh-anh-do-an-hinh-anh-mon-an-thuc-an-ngon-dep-viet-nam-the-gioi-1.jpg" alt="First slide">
+          <div class="carousel-caption d-none d-md-block text-dark">
+          <h6>Tổng liên đoàn TNTT Việt Nam: Thư kêu gọi thực hiện chiến dịch bó hoa thiêng cầu nguyện cho đại dịch sớm chấm dứt.</h6>
+        </div>
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100 img-sline" src="https://massageishealthy.com/wp-content/uploads/2019/06/hinh-anh-do-an-hinh-anh-mon-an-thuc-an-ngon-dep-viet-nam-the-gioi-2.jpg" alt="Second slide">
+          <div class="carousel-caption d-none d-md-block text-dark">
+          <h6>Thế giới trong ngày 27-7-2021: Đức Thánh Cha Phanxicô hy vọng sẽ thăm Đông Timor vào năm 2022</h6>
+        </div>
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100 img-sline" src="https://massageishealthy.com/wp-content/uploads/2019/06/hinh-anh-do-an-hinh-anh-mon-an-thuc-an-ngon-dep-viet-nam-the-gioi-3.jpg" alt="Third slide">
+          <div class="carousel-caption d-none d-md-block text-dark">
+          <h6>Thế giới trong ngày 26-7-2021: Hy vọng của Đức Thánh Cha Phanxicô nơi Olympic Tokyo 2020</h6>
+        </div>
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+    </div>
+    <div class="col-md-4 img-content-2">
+      <img style="margin-bottom: 5px;" class="w-100" src="https://massageishealthy.com/wp-content/uploads/2019/06/hinh-anh-do-an-hinh-anh-mon-an-thuc-an-ngon-dep-viet-nam-the-gioi-6.jpg">
+      <img class="w-100" src="https://massageishealthy.com/wp-content/uploads/2019/06/hinh-anh-do-an-hinh-anh-mon-an-thuc-an-ngon-dep-viet-nam-the-gioi-6.jpg">
+    </div>
+  </div>
+
+  <div class="bg-primary" style="height: 50px; margin-top: 5px;">
+    <span style="line-height: 50px; padding-left: 30px; color: white; "><b><i class="fas fa-plus-square"></i>&nbsp;TIN MỚI NHẬN</b></span>
+  </div>
+  <br>
+  <div class="row pd-10">
+    <div class="col-md-3 col-sm-6">
+      <div><img class="w-100 img-thumbnail" src="https://www.tonggiaophanhanoi.org/wp-content/uploads/2021/07/covid5-1-300x218.jpg" alt=""></div>
+      <div style="font-size: 18px; font-weight: bold;">
+        Xứ đạo thời Covid – Hoa hy vọng
+      </div>
+      <small>26/07/2021</small>
+      <div style="text-align:justify; font-size: 15px;">
+      Từ đằng xa hướng về đồi Hy Vọng, mấy đứa nhỏ đang xúm xụm lại với nhau. Tiến đến gần, tôi bật khóc trong lòng một chút. Mấy bông hoa đại được xếp tỉ mẩn thành hai chữ: “Hy Vọng” ngay dưới chân Chúa Giêsu và Đức Mẹ.
+    </div><hr>
+    </div>
+    <div class="col-md-3 col-sm-6">
+      <div><img class="w-100 img-thumbnail" src="https://www.tonggiaophanhanoi.org/wp-content/uploads/2021/07/covid5-1-300x218.jpg" alt=""></div>
+      <div style="font-size: 18px; font-weight: bold;">
+        Xứ đạo thời Covid – Hoa hy vọng
+      </div>
+      <small>26/07/2021</small>
+      <div style="text-align:justify; font-size: 15px;">
+      Từ đằng xa hướng về đồi Hy Vọng, mấy đứa nhỏ đang xúm xụm lại với nhau. Tiến đến gần, tôi bật khóc trong lòng một chút. Mấy bông hoa đại được xếp tỉ mẩn thành hai chữ: “Hy Vọng” ngay dưới chân Chúa Giêsu và Đức Mẹ.
+    </div><hr>
+    </div>
+    <div class="col-md-3 col-sm-6">
+      <div><img class="w-100 img-thumbnail" src="https://www.tonggiaophanhanoi.org/wp-content/uploads/2021/07/covid5-1-300x218.jpg" alt=""></div>
+      <div style="font-size: 18px; font-weight: bold;">
+        Xứ đạo thời Covid – Hoa hy vọng
+      </div>
+      <small>26/07/2021</small>
+      <div style="text-align:justify; font-size: 15px;">
+      Từ đằng xa hướng về đồi Hy Vọng, mấy đứa nhỏ đang xúm xụm lại với nhau. Tiến đến gần, tôi bật khóc trong lòng một chút. Mấy bông hoa đại được xếp tỉ mẩn thành hai chữ: “Hy Vọng” ngay dưới chân Chúa Giêsu và Đức Mẹ.
+    </div><hr>
+    </div>
+    <div class="col-md-3 col-sm-6">
+      <div><img class="w-100 img-thumbnail" src="https://www.tonggiaophanhanoi.org/wp-content/uploads/2021/07/covid5-1-300x218.jpg" alt=""></div>
+      <div style="font-size: 18px; font-weight: bold;">
+        Xứ đạo thời Covid – Hoa hy vọng
+      </div>
+      <small>26/07/2021</small>
+      <div style="text-align:justify; font-size: 15px;">
+      Từ đằng xa hướng về đồi Hy Vọng, mấy đứa nhỏ đang xúm xụm lại với nhau. Tiến đến gần, tôi bật khóc trong lòng một chút. Mấy bông hoa đại được xếp tỉ mẩn thành hai chữ: “Hy Vọng” ngay dưới chân Chúa Giêsu và Đức Mẹ.
+    </div><hr>
+    </div>
+  </div>
+  <br>
+  <div class="text-center" style="border-top: 10px solid brown; background: #fef3da">
+    <div class="row">
+      <div class="col-md-3 category">
+        <a href="craete.html"><button type="button" class="btn btn-primary btn-catelory"><i class="fas fa-book-medical"></i>&nbsp;Bài Đọc Thánh Lễ</button></a>
+      </div>
+      <div class="col-md-3 category">
+        <a href="dashborard2.html"><button type="button" class="btn btn-primary btn-catelory"><i class="fas fa-calendar-alt"></i>&nbsp;Lịnh Học Giáo Lý</button></a>
+      </div>
+      <div class="col-md-3 category">
+        <a href="dashboard.html"><button type="button" class="btn btn-primary btn-catelory"><i class="fas fa-clock"></i>&nbsp;Lịch Công Giáo</button></a>
+      </div>
+      <div class="col-md-3 category">
+        <a href="craete.html"><button type="button" class="btn btn-primary btn-catelory"><i class="fas fa-praying-hands"></i>&nbsp;Kinh Sáng Tối</button></a>
+      </div>
+    </div>
+  </div>
+  <div class="bg-primary" style="height: 50px; margin-top: 5px;">
+    <span style="line-height: 50px; padding-left: 30px; color: white; "><b><i class="fas fa-plus-square"></i>&nbsp;TIN MỚI NHẬN</b></span>
+  </div>
+  <br>
+  <div class="row pd-10">
+    <div class="col-md-8 col-sm-12">
+      <div class="row">
+        <div class="col-md-5"><img class="img-fluid w-100" src="https://www.tonggiaophanhanoi.org/wp-content/uploads/2021/07/covid5-1-300x218.jpg" alt=""></div>
+        <div class="col-md-7" style="font-size: 12px;">
+          <div style="font-size: 18px; font-weight: bold;">
+            Xứ đạo thời Covid – Hoa hy vọng
+          </div>
+          <small>26/07/2021</small><br><br>
+          Từ đằng xa hướng về đồi Hy Vọng, mấy đứa nhỏ đang xúm xụm lại với nhau. Tiến đến gần, tôi bật khóc trong lòng một chút. Mấy bông hoa đại được xếp tỉ mẩn thành hai chữ: “Hy Vọng” ngay dưới chân Chúa Giêsu và Đức Mẹ.
+        </div>
+      </div><hr>
+      <div class="row">
+        <div class="col-md-5"><img class="img-fluid w-100" src="https://www.tonggiaophanhanoi.org/wp-content/uploads/2021/07/covid5-1-300x218.jpg" alt=""></div>
+        <div class="col-md-7" style="font-size: 12px;">
+          <div style="font-size: 18px; font-weight: bold;">
+            Xứ đạo thời Covid – Hoa hy vọng
+          </div>
+          <small>26/07/2021</small><br><br>
+          Từ đằng xa hướng về đồi Hy Vọng, mấy đứa nhỏ đang xúm xụm lại với nhau. Tiến đến gần, tôi bật khóc trong lòng một chút. Mấy bông hoa đại được xếp tỉ mẩn thành hai chữ: “Hy Vọng” ngay dưới chân Chúa Giêsu và Đức Mẹ.
+        </div>
+      </div><hr>
+      <div class="row">
+        <div class="col-md-5"><img class="img-fluid w-100" src="https://www.tonggiaophanhanoi.org/wp-content/uploads/2021/07/covid5-1-300x218.jpg" alt=""></div>
+        <div class="col-md-7" style="font-size: 12px;">
+          <div style="font-size: 18px; font-weight: bold;">
+            Xứ đạo thời Covid – Hoa hy vọng
+          </div>
+          <small>26/07/2021</small><br><br>
+          Từ đằng xa hướng về đồi Hy Vọng, mấy đứa nhỏ đang xúm xụm lại với nhau. Tiến đến gần, tôi bật khóc trong lòng một chút. Mấy bông hoa đại được xếp tỉ mẩn thành hai chữ: “Hy Vọng” ngay dưới chân Chúa Giêsu và Đức Mẹ.
+        </div>
+      </div><hr>
+      <div class="row">
+        <div class="col-md-5"><img class="img-fluid w-100" src="https://www.tonggiaophanhanoi.org/wp-content/uploads/2021/07/covid5-1-300x218.jpg" alt=""></div>
+        <div class="col-md-7" style="font-size: 12px;">
+          <div style="font-size: 18px; font-weight: bold;">
+            Xứ đạo thời Covid – Hoa hy vọng
+          </div>
+          <small>26/07/2021</small><br><br>
+          Từ đằng xa hướng về đồi Hy Vọng, mấy đứa nhỏ đang xúm xụm lại với nhau. Tiến đến gần, tôi bật khóc trong lòng một chút. Mấy bông hoa đại được xếp tỉ mẩn thành hai chữ: “Hy Vọng” ngay dưới chân Chúa Giêsu và Đức Mẹ.
+        </div>
+      </div>
+    </div><hr>
+    <div class="col-md-4 col-sm-12">
+      <div class="bg-primary text-center" style="font-size: 20px; font-weight: bold; padding: 10px; color: white;"><i class="far fa-cross"></i> Lời Chúa Trong Thánh Lễ</div><br>
+      <div class="row">
+        <div class="col-md-4 col-5">
+          <img class="img-fluid" src="https://www.tonggiaophanhanoi.org/wp-content/uploads/2021/07/covid5-1-300x218.jpg" alt="">
+        </div>
+        <div class="col-md-8 col-7" style="font-size: 15px; font-weight: bold;">
+          Xứ đạo thời Covid – Hoa hy vọng <br>
+          <small>26/07/2021</small>
+        </div>
+      </div><hr>
+      <div class="row">
+        <div class="col-md-4 col-5">
+          <img class="img-fluid" src="https://www.tonggiaophanhanoi.org/wp-content/uploads/2021/07/covid5-1-300x218.jpg" alt="">
+        </div>
+        <div class="col-md-8 col-7" style="font-size: 15px; font-weight: bold;">
+          Xứ đạo thời Covid – Hoa hy vọng <br>
+          <small>26/07/2021</small>
+        </div>
+      </div><hr><div class="row">
+        <div class="col-md-4 col-5">
+          <img class="img-fluid" src="https://www.tonggiaophanhanoi.org/wp-content/uploads/2021/07/covid5-1-300x218.jpg" alt="">
+        </div>
+        <div class="col-md-8 col-7" style="font-size: 15px; font-weight: bold;">
+          Xứ đạo thời Covid – Hoa hy vọng <br>
+          <small>26/07/2021</small>
+        </div>
+      </div><hr><div class="row">
+        <div class="col-md-4 col-5">
+          <img class="img-fluid" src="https://www.tonggiaophanhanoi.org/wp-content/uploads/2021/07/covid5-1-300x218.jpg" alt="">
+        </div>
+        <div class="col-md-8 col-7" style="font-size: 15px; font-weight: bold;">
+          Xứ đạo thời Covid – Hoa hy vọng <br>
+          <small>26/07/2021</small>
+        </div>
+      </div><hr><div class="row">
+        <div class="col-md-12">
+          <div class="embed-responsive embed-responsive-16by9">
+          <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/c7ecVUOOgSg" allowfullscreen></iframe>
+        </div>
+        </div>
+      </div><hr>
+    </div>
+  </div>
+</div>
+<!-- /.container -->
+
+<div class="footer-dark">
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-md-3 item">
+                    <h3>Services</h3>
+                    <ul>
+                        <li><a href="#">Web design</a></li>
+                        <li><a href="#">Development</a></li>
+                        <li><a href="#">Hosting</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-6 col-md-3 item">
+                    <h3>About</h3>
+                    <ul>
+                        <li><a href="#">Company</a></li>
+                        <li><a href="#">Team</a></li>
+                        <li><a href="#">Careers</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-6 item text">
+                    <h3>Company Name</h3>
+                    <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>
+                </div>
+                <div class="col item social">
+                  <a href="#"><i class="fab fa-facebook"></i></a>
+                  <a href="#"><i class="fab fa-twitter"></i></a>
+                  <a href="#"><i class="fab fa-instagram"></i></a>
+                  <a href="#"><i class="fab fa-youtube"></i></a>
+                </div>
+            </div>
+            <p class="copyright">Company Name © 2018</p>
+        </div>
+    </footer>
+</div>
+</body>
+</html>
+<script type="text/javascript">
+  document.getElementById();
+</script>
